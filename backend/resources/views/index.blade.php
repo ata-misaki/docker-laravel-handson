ee1df423569fd25c97b19cd32553b67d3bb23305
ata <misakiata209625@gmail.com>
2021-04-20 18:40:46 +0900

    include成功

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -9,61 +9,15 @@
     <link rel="stylesheet" href="{{  asset('css/style.css') }}" />
 </head>
 <body>
-    <header>
-        <div class="menu">
-            <h1><a href="＃">九州水フォーラム</a></h1>
-            <nav>
-                <ul>
-                    <li><a href="#">新着情報<div class="en">NEWS</div></a></li>
-                    <li><a href="#">九州水フォーラムとは<div class="en">ABOUT US</div></a></li>
-                    <li><a href="#">九州の水の可能性<div class="en">POSSIBILITY</div></a></li>
-                    <li><a href="#">過去のイベント<div class="en">EVENT</div></a></li>
-                    <li><a href="#">お問い合わせ<div class="en">CONTACT</div></a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="wave">
-            
-        </div>
-        
-    </header>
+    
+
+    @include("parts.header")
+
     <div class="container">なかみ</div>
 
-    <footer>
-        <div class="menu">
-            <h1><a href="＃">九州水フォーラム</a></h1>
-            <nav>
-                <ul>
-                    <li><a href="#">新着情報<div class="en">NEWS</div></a></li>
-                    <li><a href="#">九州水フォーラムとは<div class="en">ABOUT US</div></a></li>
-                    <li><a href="#">九州の水の可能性<div class="en">POSSIBILITY</div></a></li>
-                    <li><a href="#">過去のイベント<div class="en">EVENT</div></a></li>
-                    <li><a href="#">お問い合わせ<div class="en">CONTACT</div></a></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="border"><p></p></div>
-        <div class="bottom">
-            <div class="info">
-                <div  class="address">
-                    <p>九州水フォーラム事務局</p>
-                    <p></p>〒819-0395 福岡市西区元岡744
-                </div>
-                <div  class="name">
-                    <p>九州大学大学院工学研究院 環境社会部門 生態工学研究室 | 清野・田中</p>
-                </div>
-                <div class="contact">
-                    <p>092-802-3437</p>
-                    <p>seino@civil.kyushu-u.ac.jp</p>
-                </div>
-            </div>
-            <div class="btn">
-                <a href="#" class="btn-border btn1">お問い合わせへ</a>
-                <a href="#" class="btn-border btn2">参加申し込みへ</a>
-            </div>
-        </div>
-        
-    </footer>
+    @include("parts.footer")
+
+    
 
 </body>
 </html>
---
+++ backend/resources/views/parts/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+        <div class="menu">
+            <h1><a href="＃">九州水フォーラム</a></h1>
+            <nav>
+                <ul>
+                    <li><a href="#">新着情報<div class="en">NEWS</div></a></li>
+                    <li><a href="#">九州水フォーラムとは<div class="en">ABOUT US</div></a></li>
+                    <li><a href="#">九州の水の可能性<div class="en">POSSIBILITY</div></a></li>
+                    <li><a href="#">過去のイベント<div class="en">EVENT</div></a></li>
+                    <li><a href="#">お問い合わせ<div class="en">CONTACT</div></a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="wave">
+            
+        </div>
+        
+    </header>
